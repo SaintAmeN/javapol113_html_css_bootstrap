d286e5bbd07d1abba1dd43b05f7050b1f16aa1da
Paweł Recław <pawel.reclaw@gmail.com>
2022-08-21 12:57:30 +0200

    Finished Angular version with Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// public class StudentModel {
+//    private String firstName;
+//    private String lastName;
+//    private LocalDate birthDate;
+//    private Double income;
+//    private MaritalStatus maritalStatus;
+//    private Boolean ageOver26;
+//    private Integer age;
+// }
+// DTO => Data Transfer Object
+export type StudentModel = {
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+  income: number;
+  maritalStatus: string;
+  ageOver26: boolean;
+  age: number;
+}
+
+const DEFAULT_STUDENT_FORM = {
+  firstName: '',
+  lastName: '',
+  birthDate: '2020-03-25',
+  income: 100,
+  maritalStatus: 'FREE',
+  ageOver26: true,
+  age: 25
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +37,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-html-css-bootstrap';
+  studentList: StudentModel[];
+  studentForm: StudentModel;
+
+  constructor(){
+    this.studentForm = Object.assign({}, DEFAULT_STUDENT_FORM);
+
+    this.studentList = [];
+    console.log('Loaded AppComponent')
+  }
+
+  submitClicked() {
+    console.log('Someone clicked the button!')
+
+    this.studentList.push(this.studentForm)
+    this.clear()
+    console.log(this.studentList)
+  }
+
+  clear() {
+    this.studentForm = Object.assign({}, DEFAULT_STUDENT_FORM);
+  }
+
+  removeStudent(student : StudentModel) {
+    const studentIndex = this.studentList.indexOf(student)
+    this.studentList.splice(studentIndex, 1)
+  }
+
 }
